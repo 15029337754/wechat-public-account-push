deeacb2b5818e482112692dd4df5bad8bbcde8c6
15029337754 <114278461+15029337754@users.noreply.github.com>
2022-09-24 15:16:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -50,14 +50,14 @@ export const USER_CONFIG = {
   /** 每日一言 */
   
   // 每日一言的内容类型
-  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
+  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 "动画"
   LITERARY_PREFERENCE: "",
   
   
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [oHoLa6bUVmf8PMiN5fZLiyGeGjc0
+  USERS: ["oHoLa6bUVmf8PMiN5fZLiyGeGjc0"]
     {
       // 想要发送的人的名字
       name: "乖乖",
